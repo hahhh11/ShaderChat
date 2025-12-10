a135454baeb0bd903b78b15f0ade1a473e4230d7
huangwenjie <huangwenjie@duiba.com.cn>
2025-12-10 11:14:55 +0800

    fix：build时加上资源前缀

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/*
+ * @Author: 98Precent
+ * @Date: 2025-12-09 15:35:00
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2025-12-10 11:13:58
+ * @FilePath: /ShaderChat/vite.config.ts
+ */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: process.env.BASE_URL || "/",
+	plugins: [react()],
+});
